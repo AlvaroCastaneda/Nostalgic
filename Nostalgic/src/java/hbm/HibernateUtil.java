44ab055f00c89ae900c1feafc72ef01176e8822a
tank3man <32251783+tank3man@users.noreply.github.com>
2017-11-16 09:10:44 -0600

    ya

--- Nostalgic/src/java/hbm/HibernateUtil.java
+++ Nostalgic/src/java/hbm/HibernateUtil.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,12 +5,10 @@
  */
 package hbm;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
